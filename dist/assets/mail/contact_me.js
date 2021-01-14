325da40e81a6de869cd821df6e9ac9a589598c81
Christian <christian.ledgard@utec.edu.pe>
2021-01-13 22:45:10 -0500

    web page inicial commit

---
+++ dist/assets/mail/contact_me.js
@@ -0,0 +1,80 @@
+$(function () {
+    $(
+        "#contactForm input,#contactForm textarea,#contactForm button"
+    ).jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var message = $("textarea#message").val();
+            var message2 = $("textarea#message2").val();
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+            $.ajax({
+                url: "http://167.172.139.40:8081/api/propuestas/",
+                type: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "http://re2021.tk/",
+                    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+                    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin",
+                },
+                data: JSON.stringify({"lejana":message,"realista":message2}),
+                cache: false,
+                success: function () {
+                    // Success message
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-success").append(
+                        "<strong>Muchas gracias. Hemos recibido tu mensaje.</strong>"
+                    );
+                    $("#success > .alert-success").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $("#success").html("<div class='alert alert-danger'>");
+                    $("#success > .alert-danger")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-danger").append(
+                        $("<strong>").text(
+                            "Error, al parecer nuestro servidor no se encuentra disponible. Por favor, intenta más tarde."
+                        )
+                    );
+                    $("#success > .alert-danger").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
+                },
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
---
+++ dist/js/scripts.js
@@ -0,0 +1,137 @@
+/*!
+    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
+    * Copyright 2013-2021 [object Object]
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    $(document).ready(function() {
+    if(window.location.pathname == "/respuestas.html"){
+
+        var data = $.getJSON('http://167.172.139.40:8081/api/propuestas/', function(data) {
+            $.each(
+                data,
+                function(intIndex,objValue){
+                    $('#datosJSON')
+                    .append("<tr>")
+                    .append(
+                        $("<td>" + intIndex + "</td>")
+                    )
+                    .append(
+                        $("<td>" +objValue.lejana+ "</td>")
+                    )
+                    .append(
+                        $("<td>" +objValue.realista+ "</td>")
+                    )
+                    .append("</tr>");
+                }
+                
+            );
+        });
+    }
+    // Set the date we're counting down to
+    var countDownDate = new Date("Dec 4, 2020 08:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = "Las elecciones se llevarán a cabo en " + days + " días, " + hours + " horas y "
+    + minutes + " minutos.";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "¡Vota Ya!";
+    }
+    }, 1000);
+});
+
+
+(function ($) {
+    "use strict"; // Start of use strict
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+
+
+
+
+
+
+// function ($) {  
+//       console.log("Hola");
+//       $.getJSON('http://167.172.139.40:8081/api/propuestas/', function (data) {
+//         console.log(data);
+  
+//         var items = data.items.map(function (item) {
+//           return item.key + ': ' + item.value;
+//         });
+  
+//         showData.empty();
+  
+//         if (items.length) {
+//           var content = '<li>' + items.join('</li><li>') + '</li>';
+//           var list = $('<ul />').html(content);
+//           showData.append(list);
+//         }
+//       });
+  
+//       showData.text('Loading the JSON file.');
+// }
